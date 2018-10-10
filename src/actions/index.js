cf52fab09e399fc5e6da66e1ef9a3481235c099a
Deepali2 <hdgarg@gmail.com>
2018-10-10 11:15:58 -0700

    creates post action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const FETCH_POST = 'fetch_post';
+export const CREATE_POST = 'create_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
 const API_KEY = '?key=PAPERCLIP1234567890';
@@ -20,4 +21,12 @@ export function fetchPost(id) {
     type: FETCH_POSTS,
     payload: request
   };
+}
+
+export function createPost (values) {
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+  return {
+    type: CREATE_POST,
+    payload: request
+  }
 }
