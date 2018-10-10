feae5c1083a568499ead9b527676be174468b4a3
Deepali2 <hdgarg@gmail.com>
2018-10-10 06:43:03 -0700

    receives new post

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,12 +1,21 @@
 import axios from 'axios';
 
-export const FETCH_POSTS = 'fetch_posts'
+export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
 const API_KEY = '?key=PAPERCLIP1234567890';
 
 export function fetchPosts() {
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
+  return {
+    type: FETCH_POSTS,
+    payload: request
+  };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts${id}${API_KEY}`);
   return {
     type: FETCH_POSTS,
     payload: request
